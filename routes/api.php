48fb7c9aa09415e0bb63ba852324ca4a80ee66fd
Anders Blom <andersblom@anderss-mbp.home>
2019-07-20 07:39:32 -0500

    Disable CORS blocking locally

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::group(['middleware' => ['json.response']], function () {
+Route::group(['middleware' => ['cors', 'json.response']], function () {
     /**
      * Public routes
      */
